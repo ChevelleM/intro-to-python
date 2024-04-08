09c7ee818aa7c9812426f1a1cc76fd5bbeb83956
Replit user <>
2024-04-08 15:32:10 +0000

    session5

--- session_05/exercises_5.py
+++ session_05/exercises_5.py
@@ -91,13 +91,92 @@ while x<=100:
 #         f.	Below 25 - F
 #     Ask user to enter the lesson and the marks for three lessons and print out the corresponding grades for the lesson.
 
+lesson = input("What is the lesson? ")
 
+total = 0
+gradeCount = 0
+
+while gradeCount < 3:
+    grade = int(input("What is your mark? "))
+
+    if grade < 0 or grade > 100:
+        print("It should be a number from 0 to 100")
+    else:
+      gradeCount += 1
+      total += grade
+        
+    if grade<25:
+      print("F") 
+    elif 25 <= grade < 45:
+      print("E")
+    elif 45 <= grade < 50:
+      print("D")
+    elif 50 <= grade < 60:
+      print("C")
+    elif 60 <= grade < 80:
+      print("B")
+    else:
+      print("A")
 
 # 10. Ask the user to enter the names of people who entered a prize draw. Select a random winner and print their name
 
+import random
+prize_draw_list = []
+user_input = None
 
+while user_input != "":
+    user_input = input("Please input your name to be added to the prize draw:\n")
+    if user_input != "":
+        prize_draw_list.append(user_input)
+ 
+print("Congratulations! " + random.choice(prize_draw_list) + " you are the winner of the prize draw!")
 
 # 11. Create a rock, paper, scissors game which is run against computer. 
 #    This is a game where you select either rock/paper/scissors and you compare it to your opponents choice. 
 #    Rock beats scissors, paper beats rock, scissors beats paper. If both choose the same, then you play again.
 #       + Expand this so that its best of 3 games
+
+import random
+
+print("Welcome to Rock, Paper, Scissors")
+
+user_score = 0
+computer_score = 0
+turns = 0
+
+while turns < 3:
+  user_choice = input("What is your move? (rock, paper, scissors) ")
+  computer_choice = random.choice(["rock", "paper", "scissors"])
+  print("You picked " + user_choice)
+  print("The computer picked " + computer_choice)
+  turns += 1
+  print("This is turn: " + str(turns))
+  if user_choice == "rock":
+      if computer_choice == "scissors":
+          print("You Win")
+          user_score += 1
+      elif computer_choice == "paper":
+          print("You Lose")
+          computer_score += 1
+      else:
+          print("It's a draw")
+  elif user_choice == "paper":
+      if computer_choice == "rock":
+          print("You Win")
+          user_score += 1
+      elif computer_choice == "scissors":
+          print("You Lose")
+          computer_score += 1
+      else:
+          print("It's a draw")
+  else:
+      if computer_choice == "paper":
+          print("You Win")
+          user_score += 1
+      elif computer_choice == "rock":
+          print("You Lose")
+          computer_score += 1
+      else:
+          print("It's a draw")
+
+print("Game Over! Final Score: User Score: " + str(user_score) + "\n Computer Score: " + str(computer_score))
